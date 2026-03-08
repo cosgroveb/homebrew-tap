c687fe961112099cc02fc445fdf9aa5bb61064eb
Brian Cosgrove <cosgroveb@gmail.com>
2026-03-08 05:00:30 +0000

    Add hew formula for v0.1.0

---
+++ Formula/hew.rb
@@ -0,0 +1,19 @@
+class Hew < Formula
+  desc "Minimal coding agent"
+  homepage "https://github.com/cosgroveb/hew"
+  url "https://github.com/cosgroveb/hew/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9cc0bd469d1bd708315e7be172e3f091ad4c7863947d6746c3327b45fd043442"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/hew/"
+    man1.install "doc/hew.1"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hew --version")
+  end
+end
